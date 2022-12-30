999a5caeab39fae77ddd8b3a8cd575a8ddc5e33d
hungtd2600 <hung.td172600@gmail.com>
2022-12-30 13:33:05 +0700

    fix-comment

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import GlobalStyles from "app/styles/globalstyles";
-import Landing from "app/pages/Landing";
+import LandingPage from "app/pages/LandingPage";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <GlobalStyles>
-      <Landing />
+      <LandingPage />
     </GlobalStyles>
   </React.StrictMode>
 );
